bca174756741bad84a5ca63e47a3243220208cb4
JaeHo Song <thruthesky@gmail.com>
2018-03-11 03:18:14 +0800

    adding category functionality

--- components/category/category.component.ts
+++ components/category/category.component.ts
@@ -18,31 +18,44 @@ export class CategoryComponent {
     categories: Array<CATEGORY> = [];
     form = <CATEGORY>{};
 
+    open = false;
+    inCreating = false;
     constructor(
         public cms: FirebaseCmsService
     ) {
         cms.categoryGets({ properties: ['id', 'name', 'description', 'created', 'numberOfPosts'] }).then(re => {
             console.log("categories: ", re);
-
             this.categories = re.data;
         });
     }
 
-
-
     onCategoryCreateFormSubmit(event: Event) {
         event.preventDefault();
+        this.inCreating = true;
         this.cms.categoryCreate(this.form).then( re => {
             console.log("categoryCreate: re: ", re);
-        }).catch( e => alert(e.message));
+            this.cms.categoryGet( re.data ).then( got => {
+                this.categories.push( got.data );
+                this.inCreating = false;
+            }).catch( e => alert(e) );
+        }).catch( e => {
+            this.inCreating = false;
+            alert(e.message);
+        });
         return false;
     }
 
-    onClickDeleteCategory( id ) {
-        this.cms.categoryDelete( id ).then( re => {
-            console.log("categoryDelete: re: ", re);
+    onClickDeleteCategory( category: CATEGORY ) {
+        category['inDeleting'] = true;
+        this.cms.categoryDelete( category.id ).then( re => {
+            category['inDeleting'] = false;
+            const i = this.categories.findIndex( v => v.id === category.id );
+            this.categories.splice( i, 1 );
         })
-        .catch( e => alert(e.message));
+        .catch( e => {
+            category['inDeleting'] = false;
+            alert(e.message);
+        });
     }
 
 }
